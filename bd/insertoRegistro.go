14516a8e1eda890010635d8c8e351ad2813fabed
ricardoatriana <ricardoatriana@hotmail.com>
2020-06-19 09:29:37 +0800

    Registro de usuario

--- bd/insertoRegistro.go
+++ bd/insertoRegistro.go
@@ -13,7 +13,7 @@ func InsertoRegistro(u models.Usuario) (string, bool, error) { //en los parametr
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second) // como vamos hacer operaciones de grabacion, lectura de la bd, queremos q nose quede colgada la bd asi q creamos el contexto cxt
 	defer cancel()                                                           //es una instruccion q se setea a comienzo pero se ejectua omo ultima instruccion de la funcion. el cancel cancela el widthtimeout, si no se hace asi, dnetro de la gran caja de contexto se van creando cajitas de operiaciones y si no se dan de baja van ocuapdno mucho espacio en el contexto
 
-	db := MongoCN.Database("Parcha")
+	db := MongoCN.Database("parcha")
 	col := db.Collection("usuarios")     // hay 3 colecciones; usuario, twitter y relacion. Apunta a la coleccion de usuarios de la bd de parcha
 	u.Clave, _ = EncriptarClave(u.Clave) //no se pueden grabar claves literales en las bd's es un tema de seguridad, por es se encripta
 
